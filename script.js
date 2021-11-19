fec0dd4ccb96322396589d4d026d8a345b151ea9
eryner-z <stfn.bgr@gmail.com>
2021-11-19 17:26:30 +0100

    Added function to display Book-objects

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ let testBook = new Book("Moby Dick", "Hermann Melivlle", 808, "already red");
 
 //Constructor function that makes "Book" objects.
 function Book(title, author, pages, read) {
-this.title = title;
-this.author = author;
-this.pages = pages;
-this.read = read;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
+//Push Book to myLibary.
 function addBookToLibary() {
-myLibrary.push(testBook);
-console.log(myLibrary);
+    myLibrary.push(testBook);
+    const para = document.createElement("P");
+    para.innerText = testBook.title + testBook.author + testBook.pages + testBook.read;
+    document.body.appendChild(para);
 }
