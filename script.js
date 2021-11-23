2a4b0c451e57ea5a191e4a34cf98a2fd35528c96
eryner-z <stfn.bgr@gmail.com>
2021-11-23 18:24:34 +0100

    Simple styling

--- script.js
+++ script.js
@@ -20,28 +20,44 @@ function displayLibrary() {
     nClose.classList.add("close");
     let nBook = document.createElement("div");
     nBook.classList.add("book");
+    let mTitle = document.createElement("div");
+    mTitle.classList.add("titlehead");
     let nTitle = document.createElement("div");
     nTitle.classList.add("title");
+    let mAuthor = document.createElement("div");
+    mAuthor.classList.add("authorhead");
     let nAuthor = document.createElement("div");
     nAuthor.classList.add("author");
+    let mPages = document.createElement("div");
+    mPages.classList.add("pageshead");
     let nPages = document.createElement("div");
     nPages.classList.add("pages");
+    let mRead = document.createElement("div");
+    mRead.classList.add("readhead")
     let nRead = document.createElement("div");
     nRead.classList.add("read")
 
     for (let i = 0; i < myLibrary.length; i++) {
         nClose.textContent = "X";
+        mTitle.textContent = "Title:";
         nTitle.textContent = myLibrary[i].title;
+        mAuthor.textContent = "Author:";
         nAuthor.textContent = myLibrary[i].author;
+        mPages.textContent = "Pages:";
         nPages.textContent = myLibrary[i].pages;
+        mRead.textContent = "finished?"
         nRead.textContent = myLibrary[i].red;
 
         bookShelf.appendChild(nBook);
         nBook.setAttribute("id", myLibrary.indexOf(myLibrary[i]));
         nBook.appendChild(nClose);
+        nBook.appendChild(mTitle);
         nBook.appendChild(nTitle);
+        nBook.appendChild(mAuthor);
         nBook.appendChild(nAuthor);
+        nBook.appendChild(mPages);
         nBook.appendChild(nPages);
+        nBook.appendChild(mRead);
         nBook.appendChild(nRead);
     }
     activateRemoveButton();
@@ -105,6 +121,7 @@ function activateRemoveButton() {
     });
 }
 
+//removes Book from myLibrary Array
 function removeBook(parentNodeID) {
     document.getElementById(parentNodeID).remove();
     myLibrary.splice(parentNodeID, 1);
