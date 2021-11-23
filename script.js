f00da0c569d88a12336fe9115d24cb6eb2d9c33a
eryner-z <stfn.bgr@gmail.com>
2021-11-23 20:36:54 +0100

    Deleeted leftover code

--- script.js
+++ script.js
@@ -80,7 +80,6 @@ function addBookToLibary() {
     myLibrary.push(newBook);
     displayLibrary();
     clearInput();
-
 }
 
 //Clear input fields after input.
@@ -89,7 +88,7 @@ function clearInput() {
     document.getElementById("authorInput").value = "";
     document.getElementById("pagesInput").value = "";
     document.getElementById("readCheck").value = "";
-    checkrst = document.getElementById("readCheck").checked = false;
+    document.getElementById("readCheck").checked = false;
     hideInputs();
 }
 
